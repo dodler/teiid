015cddf03444e4577eee0e31ee846deb5d27a823
dodler <avn8068@gmail.com>
2017-06-28 22:48:36 +0300

    teiid orientdb connector draft

--- connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/orientdb/OrientDBExecutionFactory.java
+++ connectors/jdbc/translator-jdbc/src/main/java/org/teiid/translator/jdbc/orientdb/OrientDBExecutionFactory.java
@@ -1,11 +1,18 @@
 package org.teiid.translator.jdbc.orientdb;
 
-import org.teiid.translator.SourceSystemFunctions;
-import org.teiid.translator.Translator;
-import org.teiid.translator.TranslatorException;
+import org.teiid.core.types.BinaryType;
+import org.teiid.language.AggregateFunction;
+import org.teiid.language.LanguageObject;
+import org.teiid.logging.LogConstants;
+import org.teiid.logging.LogManager;
+import org.teiid.translator.*;
 import org.teiid.translator.jdbc.ConvertModifier;
 import org.teiid.translator.jdbc.JDBCExecutionFactory;
-import org.teiid.translator.jdbc.orientdb.sqlfunction.AbsFunctionModifier;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.teiid.translator.jdbc.FunctionModifier.*;
 
@@ -14,10 +21,9 @@ import static org.teiid.translator.jdbc.FunctionModifier.*;
  */
 @Translator(name = "orientdb", description = "Translator for OrientDB database")
 public class OrientDBExecutionFactory extends JDBCExecutionFactory {
-    public void start throws TranslatorException{
-        super.start();
 
-        registerFunctionModifier(SourceSystemFunctions.ABS, new AbsFunctionModifier());
+    public void start() throws TranslatorException{
+        super.start();
 
         ConvertModifier convertModifier = new ConvertModifier();
         convertModifier.addTypeMapping("String", STRING);
@@ -32,10 +38,26 @@ public class OrientDBExecutionFactory extends JDBCExecutionFactory {
         convertModifier.addTypeMapping("Double", DOUBLE);
         convertModifier.addTypeMapping("Datetime", DATE);
         convertModifier.addTypeMapping("Binary", VARBINARY); // check it
-        convertModifier.addTypeMapping(""); // bigdecimal limitless ??
+        //convertModifier.addTypeMapping(""); // bigdecimal limitless ??
         convertModifier.addTypeMapping("Date", DATE);
         convertModifier.addTypeMapping("Datetime", TIME); // need only time from oeirntdb
         convertModifier.addTypeMapping("Date", TIMESTAMP); // figure out about dates etc
+    }
+
+    @Override
+    public List<String> getSupportedFunctions(){
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void initCapabilities(Connection connection) throws TranslatorException {
+        super.initCapabilities(connection);
+        // todo add version retriving
+    }
+
+    @Override // not sure on this
+    public int getTimestampNanoPrecision(){
+        return 0;
     }
 
     @Override
@@ -45,5 +67,77 @@ public class OrientDBExecutionFactory extends JDBCExecutionFactory {
         }
         return "false";
     }
+
+    /**
+     * orientdb supports generic arith operations only in eval function
+     * @return
+     */
+    @Override
+    public List<String> getDefaultSupportedFunctions(){
+        return Arrays.asList(new String[]{});
+    }
+
+    @Override
+    public NullOrder getDefaultNullOrder(){
+        return NullOrder.LOW;
+    }
+
+    @Override
+    public List<?> translate(LanguageObject obj, ExecutionContext context){
+        AggregateFunction t = (AggregateFunction) obj;
+        LogManager.logDetail(LogConstants.CTX_CONNECTOR, t.getName());
+        return super.translate(obj, context);
+    }
+
+    /*
+    started here group by
+    finished here supportsOrCriteria,
+
+    * */
+
+    /**
+     * orientdb performs query, but result is not sorted
+     * @return
+     */
+    @Override
+    public boolean supportsOrderByUnrelated(){
+        return false;
+    }
+
+    @Override
+    public boolean supportsQuantifiedCompareCriteriaAll(){
+        return false;
+    }
+
+    /**
+     * this can be bug actually, because afaik orientdb doesn't support having
+     * @return
+     */
+    @Override
+    public boolean supportsScalarSubqueries(){
+        return true;
+    }
+
+    @Override
+    public boolean supportsSearchedCaseExpressions(){
+        return false;
+    }
+
+    @Override
+    public boolean supportsSelfJoins(){
+        return false;
+    }
+
+    @Override
+    public boolean supportsInlineViews(){
+        return true;
+    }
+
+    @Override
+    public boolean supportsQuantifiedCompareCriteriaSome(){
+        return true;
+    }
+
+
 
 }
